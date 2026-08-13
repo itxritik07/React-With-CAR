33915a429e375c5b0c5f8eca0da48d2575bb5881
Ritik <alphagem47@gmail.com>
2026-08-13 08:48:28 +0530

    add currency converter project

---
+++ 06_currency_converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+// https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json - updated API currency.
+import { useEffect, useState } from "react";
+
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+        // console.log("Data: ", data)
+    }, [currency]);
+    return data
+};
+
+export default useCurrencyInfo
